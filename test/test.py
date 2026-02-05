0763505205835b4f05f8c8f6444285604014c82c
hu8785 <patilaku96@gmail.com>
2026-02-04 20:35:42 -0500

    Fix gl_test: properly indent all code in async function

--- test/test.py
+++ test/test.py
@@ -1,53 +1,55 @@
-# Test file for D flip-flop cocotb simulation
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
+
 @cocotb.test()
 async def test_project(dut):
     dut._log.info("Start")
 
-# Set the clock period to 10 us (100 KHz)
-clock = Clock(dut.clk, 10, units="us")
-cocotb.start_soon(clock.start())
+    # Set the clock period to 10 us (100 KHz)
+    clock = Clock(dut.clk, 10, units="us")
+    cocotb.start_soon(clock.start())
 
-# Reset
-dut._log.info("Reset")
-dut.ena.value = 1
-dut.ui_in.value = 0
-dut.uio_in.value = 0
-dut.rst_n.value = 0
-await ClockCycles(dut.clk, 10)
-dut.rst_n.value = 1
+    # Reset
+    dut._log.info("Reset")
+    dut.ena.value = 1
+    dut.ui_in.value = 0
+    dut.uio_in.value = 0
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 10)
+    dut.rst_n.value = 1
 
-dut._log.info("Test project behavior")
+    dut._log.info("Test project behavior")
 
-# Set the input values you want to test
-dut.ui_in[0].value = 0
-dut.rst_n.value = 1
-# Wait for some clock cycles to see the output values
-await ClockCycles(dut.clk, 20)
+    # Set the input values you want to test
+    dut.ui_in[0].value = 0
+    dut.rst_n.value = 1
 
-# The following assersion is just an example of how to check the output values.
-# Change it to match the actual expected output of your module:
-# Asserting the output is not required but encouraged.
-# assert dut.uo_out[0].value == 50
+    # Wait for some clock cycles to see the output values
+    await ClockCycles(dut.clk, 20)
 
-# Keep testing the module by changing the input values, waiting for
-# one or more clock cycles, and asserting the expected output values.
+    # The following assersion is just an example of how to check the output values.
+    # Change it to match the actual expected output of your module:
+    # Asserting the output is not required but encouraged.
+    # assert dut.uo_out[0].value == 50
 
-dut.rst_n.value = 0
-await ClockCycles(dut.clk, 10)
+    # Keep testing the module by changing the input values, waiting for
+    # one or more clock cycles, and asserting the expected output values.
 
-dut.ui_in[0].value = 1
-await ClockCycles(dut.clk, 20)
-dut.rst_n.value = 1
-await ClockCycles(dut.clk, 18)
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 10)
 
-dut.ui_in[0].value = 0
-await ClockCycles(dut.clk, 1)
+    dut.ui_in[0].value = 1
+    await ClockCycles(dut.clk, 20)
 
-dut.ui_in[0].value = 1
-await ClockCycles(dut.clk, 20)
+    dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 18)
 
-dut.ui_in[0].value = 0
-await ClockCycles(dut.clk, 10)
+    dut.ui_in[0].value = 0
+    await ClockCycles(dut.clk, 1)
+
+    dut.ui_in[0].value = 1
+    await ClockCycles(dut.clk, 20)
+
+    dut.ui_in[0].value = 0
+    await ClockCycles(dut.clk, 10)
